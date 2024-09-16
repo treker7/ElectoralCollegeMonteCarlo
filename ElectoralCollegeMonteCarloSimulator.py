47671ae661de12f33ba801732e1d60eeab0032c7
Treker Battig <treker.battig@protonmail.com>
2024-09-16 02:08:45 -0400

    Added missing case for electoral college vote ties (269 - 269)

--- ElectoralCollegeMonteCarloSimulator.py
+++ ElectoralCollegeMonteCarloSimulator.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import statistics
 
 from ElectoralCollege2024 import *
 
@@ -10,6 +11,12 @@ class ElectoralCollegeResult():
 
     def isRedWin(self):
         return self.redElectoralPoints >= 270
+    
+    def isBlueWin(self):
+        return self.blueElectoralPoints >= 270
+    
+    def isTie(self):
+        return (self.blueElectoralPoints < 270) and (self.redElectoralPoints < 270)
 
 class ElectoralCollegeMonteCarloSimulator():
     def __init__(self, electoralCollegeStates, consensusPointsRed = 0, consensusPointsBlue = 0):
@@ -53,18 +60,28 @@ def main():
         simulator = ElectoralCollegeMonteCarloSimulator(ALL_ELECTORAL_COLLEGE_STATES)
     else:
         simulator = ElectoralCollegeMonteCarloSimulator(BATTLEGROUND_ELECTORAL_COLLEGE_STATES, CONSENSUS_ELECTORAL_POINTS_RED, CONSENSUS_ELECTORAL_POINTS_BLUE)
-    
+
     print("<<< Electoral College Monte Carlo Simulations >>>\n")
     simulations = simulator.runMonteCarloSimulations(numIterations)
 
-    probabilityRedWin = len(list(filter(lambda s: s.isRedWin(), simulations))) / len(simulations)
-    probabilityBlueWin = 1 - probabilityRedWin
+    simulationsRedWin = list(filter(lambda s: s.isRedWin(), simulations))
+    simulationsBlueWin = list(filter(lambda s: s.isBlueWin(), simulations))
+    simulationsTie = list(filter(lambda s: s.isTie(), simulations))
 
-    print(f"Expected value of red points = {simulator.getExpectedPointsRed()}")
-    print(f"Expected value of blue points = {simulator.getExpectedPointsBlue()}")
+    probabilityRedWin = len(simulationsRedWin) / len(simulations)
+    probabilityBlueWin = len(simulationsBlueWin) / len(simulations)
+    probabilityTie = len(simulationsTie) / len(simulations)
 
-    print(f"Probability red win = {100 * probabilityRedWin:.2f}%")
-    print(f"Probability blue win = {100 * probabilityBlueWin:.2f}%")
+    medianElectoralPointsRedWin = statistics.median([s.redElectoralPoints for s in simulationsRedWin])
+    medianElectoralPointsBlueWin = statistics.median([s.blueElectoralPoints for s in simulationsBlueWin])
+
+    print(f"Probability of red win = {100 * probabilityRedWin:.2f}%")
+    print(f"Probability of blue win = {100 * probabilityBlueWin:.2f}%")
+    print(f"Probability of tie = {100 * probabilityTie:.2f}%")
+    print()
+
+    print(f"Median electoral college vote for red wins = {medianElectoralPointsBlueWin}")
+    print(f"Median electoral college vote for blue wins = {medianElectoralPointsRedWin}")
 
     return 0
 
--- README.md
+++ README.md
@@ -5,15 +5,17 @@ Simulates the 2024 presidential election with Monte Carlo simulations.
 
 ```
 > python ElectoralCollegeMonteCarloSimulator.py
-Usage: ElectoralCollegeMonteCarloSimulator.py <num_iterations>
+Usage: ElectoralCollegeMonteCarloSimulator.py <num_iterations> [-a]
 
-> python ElectoralCollegeMonteCarloSimulator.py 100000
+> python ElectoralCollegeMonteCarloSimulator.py 1000000
 <<< Electoral College Monte Carlo Simulations >>>
 
-Expected value of red points = 266.84
-Expected value of blue points = 271.16
-Probability red win = 45.29%
-Probability blue win = 54.71%
+Probability of red win = 45.28%
+Probability of blue win = 53.17%
+Probability of tie = 1.55%
+
+Median electoral college vote for red wins = 283.0
+Median electoral college vote for blue wins = 281.0
 
 > 
 ```
